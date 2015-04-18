e84efd5363b32295ef4cfe88976aeb28608ff574
Artem Novichkov <novichkoff93@gmail.com>
2015-04-18 20:58:21 +0600

    Parse SDK configured

--- Paws/AppDelegate.swift
+++ Paws/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        Parse.setApplicationId("egOZILSLbGWgJaIpJwD9HYO291aNZWotzZbzeNgu", clientKey: "2HJNMUwtI0GlOjZC5hqYFlEHQ0YEsm9RTnv6fzpP")
         return true
     }
 
---
+++ Paws/CatsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CatsTableViewController.swift
+//  Paws
+//
+//  Created by Артем on 18.04.15.
+//  Copyright (c) 2015 Artem Novichkov. All rights reserved.
+//
+
+import Cocoa
+
+class CatsTableViewController: PFQueryTableViewController {
+
+}
