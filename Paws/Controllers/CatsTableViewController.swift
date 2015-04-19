a7168dd44d661b8ed9704857287a34d4af3d9ade
Artem Novichkov <novichkoff93@gmail.com>
2015-04-19 10:17:33 +0600

    Like function added

--- Paws/Controllers/CatsTableViewController.swift
+++ Paws/Controllers/CatsTableViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class CatsTableViewController: PFQueryTableViewController {
+    let cellIdentifier:String = "CatsTableViewCell"
     override init(style: UITableViewStyle, className: String?) {
         super.init(style: style, className: className)
         self.pullToRefreshEnabled = true
         self.paginationEnabled = false
         self.objectsPerPage = 25
         self.parseClassName = className
+        self.tableView.rowHeight = 350
+        self.tableView.allowsSelection = false
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        self.tableView.registerNib(UINib(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
+        
+        
     }
     required init!(coder aDecoder: NSCoder!) {
         fatalError("NSCoding not supported")
@@ -29,13 +38,34 @@ class CatsTableViewController: PFQueryTableViewController {
         return query
     }
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath, object: PFObject?) -> PFTableViewCell? {
-        let cellIdentifier:String = "Cell"
-        var cell:PFTableViewCell? = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? PFTableViewCell
+        var cell:CatsTableViewCell? = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? CatsTableViewCell
         if (cell == nil) {
-            cell = PFTableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellIdentifier)
+            cell = NSBundle.mainBundle().loadNibNamed(cellIdentifier, owner: self, options: nil)[0] as? CatsTableViewCell
         }
+        cell?.parseObject = object
         if let pfObject = object {
-            cell?.textLabel?.text = pfObject["name"] as? String
+            cell?.catNameLabel?.text = pfObject["name"] as? String
+            var votes:Int? = pfObject["votes"] as? Int
+            if votes == nil {
+                votes = 0
+            }
+            cell?.catVotesLabel?.text = "\(votes!) votes"
+            var credit:String? = pfObject["cc_by"] as? String
+            if credit != nil {
+                cell?.catCreditLabel?.text = "\(credit!) CC 2.0"
+            }
+            cell?.catImageView?.image = nil
+            if var urlString:String? = pfObject["url"] as? String {
+                var url:NSURL? = NSURL(string: urlString!)
+                if var url:NSURL? = NSURL(string: urlString!) {
+                    var error:NSError?
+                    var request:NSURLRequest = NSURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.ReturnCacheDataElseLoad, timeoutInterval: 5.0)
+                    NSOperationQueue.mainQueue().cancelAllOperations()
+                    NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response:NSURLResponse!, imageData:NSData!, error:NSError!) -> Void in
+                        cell?.catImageView?.image = UIImage(data: imageData)
+                    })
+                }
+            }
         }
         return cell
     }
--- Paws/Views/CatsTableViewCell.swift
+++ Paws/Views/CatsTableViewCell.swift
@@ -14,17 +14,36 @@ class CatsTableViewCell: PFTableViewCell {
     @IBOutlet weak var catVotesLabel: UILabel!
     @IBOutlet weak var catNameLabel: UILabel!
     @IBOutlet weak var catCreditLabel: UILabel!
+    @IBOutlet weak var catPawIcon: UIImageView!
     
+    var parseObject:PFObject?
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        let gesture = UITapGestureRecognizer(target: self, action: Selector("onDoubleTap:"))
+        gesture.numberOfTapsRequired = 2
+        contentView.addGestureRecognizer(gesture)
+        catPawIcon?.hidden = true
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func onDoubleTap(sender: AnyObject) {
+        if (parseObject != nil) {
+            if var votes:Int? = parseObject?.objectForKey("votes") as? Int {
+                votes!++
+                parseObject!.setObject(votes!, forKey: "votes")
+                parseObject!.saveInBackground()
+                catVotesLabel?.text = "\(votes!) votes"
+            }
+        }
+        
+        catPawIcon?.hidden = false
+        catPawIcon?.alpha = 1.0
+        
+        UIView.animateWithDuration(1.0, delay: 1.0, options:nil, animations: {
+            self.catPawIcon?.alpha = 0
+            }, completion: {
+                (value:Bool) in
+                self.catPawIcon?.hidden = true
+        })
     }
-    
 }
